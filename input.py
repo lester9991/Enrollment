37f880fa5d87275c741fcce4c9a1ab61e909a01f
lester9991 <lesterdsouza820@gmail.com>
2022-06-05 12:23:44 +0530

    1

--- input.py
+++ input.py
@@ -2,21 +2,26 @@ import pandas as pd
 from pprint import pprint
 df = pd.read_csv('bio.csv')
 YEAR = '22'
-BLOCK = '01'
+BLOCK = '1'
 batches = ['a', 'b', 'c', 'd']
 batches_id = {}
-for batch batc
+for batch in batches:
+    num = str(batches.index(batch) + 1).zfill(2)
+    batches_id[batch] = num
+# pprint(batches_id)
 batch_sort = {}
 # print(df.to_string())
 for batch in batches:
     batch_1 = df.index[df.batch == batch]
     batch_sort[batch] = batch_1
 dict = {}
+register = []
 for batches in batch_sort:
     # print(batch_sort[batches])
     indices = batch_sort[batches]
     names = []
     dict1 = {}
+
     for index in indices:
         name = df.at[index, 'name']
         names.append(name)
@@ -24,11 +29,17 @@ for batches in batch_sort:
     # print(dict1)
     names.sort()
     for name in names:
-
-        dict[dict1[name]] = {'name': name, 'batch': batches}
-pprint(dict)
-
-
+        num = str(names.index(name) + 1).zfill(2)
+        reg = YEAR + BLOCK +batches_id[batches] + num
+        dict[dict1[name]] = {'name': name, 'batch': batches, 'register': reg}
+dict = sorted(dict.items())
+# print(dict)
+for index in dict:
+    register.append(int(index[1]['register']))
+# print(register)
+df['register no.'] = register
+df = df.sort_values('register no.')
+print(df)
 
 
 
